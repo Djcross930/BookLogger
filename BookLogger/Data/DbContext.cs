d4d8322976392aa02092f16cf34a78c4d166faf1
Dan Cross <Dancross96@gmail.com>
2023-06-23 09:19:37 -0500

    added dbcontext

---
+++ BookLogger/Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookLogger.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models;
+
+namespace BookLogger.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
